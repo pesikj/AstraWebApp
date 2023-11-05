f960806d8f265265b02d804bbefc1809c6e59f2a
Jiří Pešík <jiri.pesik@outlook.cz>
2023-11-05 21:31:17 +0100

    Initial commit

--- inventory_management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("upload/", views.FileFieldFormView.as_view(), name="upload_file"),
-    path("inventory-table/", views.InventoryItemListView.as_view(), name="inventory_table"),
-]
